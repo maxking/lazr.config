ac297c537d23624788855922c084e98584616212
Gary Poster <gary.poster@canonical.com>
2008-12-19 14:36:16 -0500

    Add in LGPL license headers

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 # Copyright 2008 Canonical Ltd.  All rights reserved.
+#
+# This file is part of lazr.config.
+#
+# lazr.config is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# lazr.config is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with lazr.config.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup, find_packages
 
@@ -26,7 +41,7 @@ setup(
     zip_safe=False,
     maintainer='LAZR Developers',
     maintainer_email='lazr-developers@lists.launchpad.net',
-    description=open('README.txt').readlines()[0].strip(),
+    description=open('README.txt').readline().strip(),
     long_description=generate(
         'src/lazr/config/README.txt',
         'src/lazr/config/CHANGES.txt'),
